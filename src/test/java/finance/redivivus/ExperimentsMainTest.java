235c9d9170af60331862996b7588d56d1c9ba9ab
Daniel Lewandowski <daniel.lewandowski@outlook.com>
2022-06-05 12:04:47 +0200

    added test

--- src/test/java/finance/redivivus/ExperimentsMainTest.java
+++ src/test/java/finance/redivivus/ExperimentsMainTest.java
@@ -31,61 +31,62 @@ class ExperimentsMainTest {
         final var topology = builder.build();
 
         // setup test driver
-        final var testDriver = new TopologyTestDriver(topology);
+        try (final var testDriver = new TopologyTestDriver(topology)) {
 
-        final var topicProcessed = testDriver
-                .createInputTopic(
-                        ExperimentsMain.topicProcessed,
-                        Serdes.String().serializer(),
-                        ExperimentsMain.serdeOrder.serializer()
-                );
+            final var topicProcessed = testDriver
+                    .createInputTopic(
+                            ExperimentsMain.topicProcessed,
+                            Serdes.String().serializer(),
+                            ExperimentsMain.serdeOrder.serializer()
+                    );
 
-        final var topicSubmitted = testDriver
-                .createInputTopic(
-                        ExperimentsMain.topicSubmitted,
-                        Serdes.String().serializer(),
-                        ExperimentsMain.serdeOrder.serializer()
-                );
+            final var topicSubmitted = testDriver
+                    .createInputTopic(
+                            ExperimentsMain.topicSubmitted,
+                            Serdes.String().serializer(),
+                            ExperimentsMain.serdeOrder.serializer()
+                    );
 
-        final var topicPortfolio = testDriver
-                .createOutputTopic(
-                        ExperimentsMain.topicPortfolio,
-                        ExperimentsMain.serdeInstrument.deserializer(),
-                        ExperimentsMain.serdeBookEntry.deserializer()
-                );
+            final var topicPortfolio = testDriver
+                    .createOutputTopic(
+                            ExperimentsMain.topicPortfolio,
+                            ExperimentsMain.serdeInstrument.deserializer(),
+                            ExperimentsMain.serdeBookEntry.deserializer()
+                    );
 
-        Order orderSold = new Order(
-                OrderState.PROCESSED,
-                Instruments.cash,
-                new Quantity(100L),
-                Instruments.cash,
-                new Quantity(0L)
-        );
+            Order orderSold = new Order(
+                    OrderState.PROCESSED,
+                    Instruments.cash,
+                    new Quantity(100L),
+                    Instruments.cash,
+                    new Quantity(0L)
+            );
 
-        Order orderBuy = new Order(
-                OrderState.SUBMITTED,
-                Instruments.stock1,
-                new Quantity(1L),
-                Instruments.cash,
-                new Quantity(10L)
-        );
+            Order orderBuy = new Order(
+                    OrderState.SUBMITTED,
+                    Instruments.stock1,
+                    new Quantity(1L),
+                    Instruments.cash,
+                    new Quantity(10L)
+            );
 
-        Order orderSell = new Order(
-                OrderState.SUBMITTED,
-                Instruments.cash,
-                new Quantity(30L),
-                Instruments.stock1,
-                new Quantity(5L)
-        );
+            Order orderSell = new Order(
+                    OrderState.SUBMITTED,
+                    Instruments.cash,
+                    new Quantity(30L),
+                    Instruments.stock1,
+                    new Quantity(5L)
+            );
 
-        topicProcessed.pipeInput("whatever", orderSold);
-        topicSubmitted.pipeInput("whatever", orderBuy);
-        topicSubmitted.pipeInput("whatever", orderSell);
+            topicProcessed.pipeInput("whatever", orderSold);
+            topicSubmitted.pipeInput("whatever", orderBuy);
+            topicSubmitted.pipeInput("whatever", orderSell);
 
-        final var record1 = topicPortfolio.readKeyValue();
-        final var record2 = topicPortfolio.readKeyValue();
-        final var record3 = topicPortfolio.readKeyValue();
+            final var record1 = topicPortfolio.readKeyValue();
+            final var record2 = topicPortfolio.readKeyValue();
+            final var record3 = topicPortfolio.readKeyValue();
 
-        final var so = new String(ExperimentsMain.serdeOrder.serializer().serialize("", orderSold));
+            final var so = new String(ExperimentsMain.serdeOrder.serializer().serialize("", orderSold));
+        }
     }
 }
